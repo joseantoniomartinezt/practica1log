dbb2e7973e6a91335446484f478cdf6e34c6dc07
joseantonio <joseantoniomartinez1904@gmail.com>
2021-11-30 11:58:10 +0100

    first commit

---
+++ src/test/java/com/everis/practicaLogs/PracticaLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.practicaLogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
